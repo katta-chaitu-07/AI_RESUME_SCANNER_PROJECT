1830b5fb6d2403458247e6e90385da2e5c043288
unknown <chaitukatta62@gmail.com>
2026-09-08 18:24:23 +0530

    Day - 1 : project setup and initial streamlit app

---
+++ APP/app.py
@@ -0,0 +1,31 @@
+# step - 1 : - Import the streamlit library
+
+import streamlit as st
+
+# step up the page layout 
+
+st.set_page_config(
+
+    page_title = "AI RESUME SCANNER",
+    page_icon = "📄",
+    layout = "wide"
+)
+
+
+# Title 
+
+st.title("AI RESUME SCANNER ")
+
+
+# info about the project
+
+st.write(
+    "AI Powered system for analyzing resumes "
+     "and matching them with the job descriptions "
+     )
+
+# info
+
+st.info(" 🚧 Project under development")
+
+
